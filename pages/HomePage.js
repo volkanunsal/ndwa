03bef6a10d446f9c5b14a0ba48fafd237817aab0
Volkan Unsal <spocksplanet@gmail.com>
2015-06-10 16:51:28 -0400

    Redo the home page

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -3,108 +3,151 @@ import {Link} from 'react-router';
 var basePath = '/';
 
 export default class SectionPage extends React.Component {
-  handleCTAClick(){
-    window.scrollTo(0,React.findDOMNode(this.refs.createSection).getBoundingClientRect().top)
-  }
+
 
   render() {
     return <div style={{position: 'relative'}}>
-      <a className='logo' style={{position: 'absolute', left: 0, top: 0, height: 50, width: 50, marginBottom: 0}}>
-        <img src={require('../images/logo.png')}/>
-      </a>
-      <div style={{backgroundColor:'white', marginBottom: 0, height: 650, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <div>
-          <h4 className='text-center'>In-House Agreements: Generate your own work agreement effortlessly.</h4>
-          <div className='container' style={{position: 'relative'}}>
-            <h2 className='text-center'>Did you know that as of April 1st, 2015 Massachusetts requires a written agreement if you employ or you are employed as a domestic worker?</h2>
-            <img src={require('../images/plane.png')} style={{position: 'absolute', top: -80, right: -50}} className='visible-lg visible-md'/>
+        <section id='hs-01' className='home-section bg-brand-black'>
+          <div className='container-fluid text-center'>
+            <img src={require('../images/home_01.png')}/>
+            <img src={require('../images/pencil_paper_main.png')} style={{position: 'relative', bottom: -120}}/>
           </div>
-          <div style={{display: 'flex', justifyContent: 'center'}}>
-            <ul className='list-inline'>
-              <li><a onClick={this.handleCTAClick.bind(this)} className='btn btn-link btn-lg' style={{borderRadius: 0, border: 'solid 1px black', color: 'black'}}>No, I Didn’t</a></li>
-              <li><a onClick={this.handleCTAClick.bind(this)} className='btn btn-link btn-lg' style={{borderRadius: 0, border: 'solid 1px black', color: 'black'}}>Yes, I Did</a></li>
-            </ul>
+        </section>
+
+        <section id='hs-02' className='home-section bg-brand-blue'>
+          <div className='container text-center'>
+
+            <Link to='section' params={{sectionName: '1'}} className='btn btn-link btn-lg btn-custom' style={{marginTop: 80, marginBottom: 80}}>
+              Get Started!
+            </Link>
+
+            <p style={{fontSize: '3em'}}>
+              With In-House Agreements, creating your own work agreement is effortless. Step-by-step you’ll find the information you need, right at your fingertips. Simply answer the questions and fill in the blanks to fit your situation. Print your completed agreement, and CONGRATS you’re done!
+            </p>
           </div>
-        </div>
-      </div>
+        </section>
+        <section id='hs-03' className='home-section bg-brand-orange'>
+          <div className='container'>
+            <table style={{color: 'white'}}>
+              <tbody>
+                <tr>
+                  <td>
+                    <div className='text-center'><img src={require('../images/scale.png')}/></div>
+                    <h2>Keeps You Informed </h2>
+                  </td>
+                  <td>
+                    <div className='text-center'><img src={require('../images/arrow.png')}/></div>
+                    <h2>Straightforward</h2>
+                  </td>
+                  <td>
+                    <div className='text-center'><img src={require('../images/speech_bubbles.png')}/></div>
+                    <h2>Improves Communication</h2>
+                  </td>
+                </tr>
+                <tr style={{color: 'white', fontSize: '1.6em'}}>
+                  <td>
+                    <p>We work with the folks who helped implement the Bill of Rights, because agreements can feel complicated.</p>
+                  </td>
+                  <td>
+                    <p>We use plain language not legalese, because agreements can be daunting and confusing. </p>
+                  </td>
+                  <td>
+                    <p>You can avoid problems in the future, because agreements resolve potential conflicts at the outset.</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
 
-      <a name='create' ref='createSection' />
-      <div style={{backgroundColor: '#66666f', color: 'white', marginBottom: 0, height: 800, fontSize: '2em', paddingTop: 40}}>
-        <h1 className='text-center'> Create, Comply and Print!</h1>
-        <h3 className='text-center'>Constructing your own work agreement just got easier.</h3>
-
-        <ul style={{width: 1000, margin: '80px auto'}}>
-          <li>
-            It’s Straightforward.
-            <br/>
-            We use plain language not legalese, because agreements can be daunting and confusing.
-          </li>
-
-          <li>
-            It Helps You Comply with the Law.
-            <br/>
-            We work with the folks who helped implement the Bill of Rights, because agreements can feel complicated.
-          </li>
-
-          <li>
-            It Improves Communication.
-            <br/>
-            You can avoid problems in the future, because agreements resolve potential conflicts at the outset.
-          </li>
-        </ul>
-      </div>
-      <div style={{backgroundColor: '#ffe599', marginBottom: 0, height: 600, fontSize: '2em', paddingTop: 0, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-        <div>
-          <h1 className='text-center'>How does it work?</h1>
-          <ol style={{width: 1000, margin: '0 auto'}}>
-            <li>Click “I Need an Agreement”</li>
-            <li>Step-by-Step you’ll find the information you need, right at your fingertips</li>
-            <li>Complete the agreement to fit your situation.</li>
-            <li>CONGRATS! You’re done. Print your agreement.</li>
-          </ol>
-          <div style={{display: 'flex', justifyContent: 'center', marginTop: 40}}>
-            <Link to='section' style={{borderRadius: 0, border: 'solid 1px black', color: 'black', fontSize: '1.6em'}} className='btn btn-link btn-lg' params={{sectionName: 1}}>I Need an Agreement!</Link>
           </div>
-        </div>
-      </div>
+        </section>
+        <section id='hs-04' className='home-section bg-brand-blue'>
+           <div className='container' style={{minHeight: 250}}>
+            <div className='row' style={{display: 'flex', alignItems: 'stretch', justifyContent: 'center', minHeight: 250, paddingBottom: 40}}>
+              <div className='col-lg-6' style={{paddingTop: 20}}>
+                <h2 style={{lineHeight:'1.4em', fontSize: '3em'}}>Share In-House Agreements</h2>
+                <ul className='list-inline'>
+                  <li><img src={require('../images/circle_FB.png')}/></li>
+                  <li><img src={require('../images/circle_mail.png')}/></li>
+                  <li><img src={require('../images/circle_tweet.png')}/></li>
+                </ul>
+              </div>
+              <div className='col-lg-6' style={{borderLeft: 'solid 1px white', fontSize: '2em', paddingTop: 20, paddingLeft: 40}} >
+                <p>We’re just getting started! Sign-up for news and updates</p>
+                <form id='signup-form'>
+                  <input type='text' className='form-control input-lg' placeholder='Name'/>
+                  <input type='text' className='form-control input-lg' placeholder='Email Address'/>
+                  <a href='javascript:;'>
+                    <img src={require('../images/circle_check.png')}/>
+                  </a>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id='hs-05' className='home-section bg-brand-yellow' style={{padding:'10px 0 20px'}}>
+          <div className='container'>
+            <h1 style={{fontSize: '4em'}}>About the project</h1>
+          </div>
+        </section>
+        <section id='hs-06' className='home-section bg-white' style={{color: 'black', paddingBottom: 40, fontSize: '2em'}}>
+          <div className='container'>
+            <p>
+              We imagined In-House Agreements as a tool that makes it easier to create a work agreement. As a
+              non-profit venture of Fair Care Labs, we are always looking for innovative ways to solve problems
+              facing domestic workers and their employers. With Massachusetts’ new requirement for a written
+              work agreement, we wondered: how will employers and employees, without HR departments of their
+              own, create agreements that comply with the law?
+            </p>
+            <p>
+              We think that the process of creating an agreement should be friendly, easy to understand,
+              informative and feel inclusive. You should know or have access to both parties’ rights and
+              responsibilities as you create your document, and it should be written simply and clearly so you can
+              make informed decisions about your working relationship. It’s time that creating a contract makes
+              sense to everyone, so you can agree amicably, easily and with dignity, like you wanted all along.
+            </p>
+          </div>
+        </section>
+        <section id='hs-07' className='home-section bg-brand-black' style={{padding:'10px 0 20px'}}>
+          <div className='container'>
+            <h1 style={{fontSize: '4em', marginBottom: 40}}>Additional Information</h1>
+            <p style={{fontSize: '2em'}}>
+              If you’d like to learn more about the Massachusetts requirement for a written
+              work agreement, or more information about the Bill of Rights in general, we
+              suggest you check out the following resources:
+            </p>
 
-      <div className='jumbotron' style={{backgroundColor: 'white', marginBottom: 0}}>
-        <h2 className='text-center'>About the Project</h2>
-        <div className='container'>
-          <p>{"We imagined In-house Agreements as a tool that makes it easier to create a work agreement. As a non-profit venture of Fair Care Labs, we are always looking for innovative ways to solve problems facing domestic workers and their employers. With Massachusetts’ new requirement for a written work agreement, we wondered: how will employers and employees, without HR departments of their own, create agreements that comply with the law? We looked around and found sample work agreements, helpful hints, and places to get started, but we wanted more."}</p>
-          <p>{"We think that the process of creating an agreement should be friendly, easy to understand, informative and feel inclusive. You should know or have access to both parties’ rights and responsibilities as you create your document, and it should be written simply and clearly so you can make informed decisions about your working relationship. It’s time that creating a contract makes sense to everyone, so you can agree amicably, easily and with dignity, like you wanted all along."}</p>
-          <h4 className='text-center'>If you think everyone should find it easier to agree, then share the crap outta me</h4>
-        </div>
+            <div className='row' style={{marginTop: 100, width: '90%', marginLeft: 'auto', marginRight: 'auto', paddingBottom: 90}}>
+              <div className='col-lg-6'>
+                <p className='text-center' style={{marginBottom: 80}}>
+                  <img src={require('../images/terms.png')}/>
+                </p>
 
-        <div style={{display: 'flex', justifyContent: 'center'}}>
-          <ul className='list-inline' style={{alignSelf: 'center'}}>
-            <li><a className='btn btn-link btn-lg' style={{borderRadius: 0, border: 'solid 1px black', color: 'black'}}>Email addy</a></li>
-            <li><a className='btn btn-link btn-lg' style={{borderRadius: 0, border: 'solid 1px black', color: 'black'}}>Stay in touch</a></li>
-          </ul>
-        </div>
-      </div>
-      <div style={{backgroundColor: '#d89f39', marginBottom: 0, paddingBottom: 40}}>
-        <div className='text-center' style={{position: 'relative', top: -40}} >
-          <img src={require('../images/logo2.png')} />
-        </div>
-        <h3 className='text-center' style={{marginTop:0}}>
-          Partners
-          <br/>
-          NDWA, HIH, MDWC
-        </h3>
-
-      </div>
-      <div style={{backgroundColor: 'white', marginBottom: 0, fontSize: '2em', paddingTop: 40, paddingBottom: 120}}>
-        <h1 className='text-center'>
-          TOS
-        </h1>
-        <div className='container'>
-          <p>{"Disclaimer: You can use the ideas/terms set forth in the sample agreement on the website to create your own agreement. Be sure to complete/fill in your agreement to fit your own situation, and be as specific as possible about benefits, time off, rights and obligations, and so on."}</p>
-        </div>
-        <div style={{display: 'flex', justifyContent: 'center', marginTop: 40}}>
-          <Link to='section' style={{borderRadius: 0, border: 'solid 1px black', color: 'black', fontSize: '1.6em'}} className='btn btn-link btn-lg' params={{sectionName: 1}}>Get Started</Link>
-        </div>
-      </div>
+                <p style={{fontSize: '2em'}}>Review the terms
+                  or conditions of
+                  employment for
+                  domestic workers</p>
+              </div>
+              <div className='col-lg-6'>
+                <div className='row' style={{borderBottom: 'solid 1px white', paddingBottom: 40}}>
+                  <div className='col-lg-4'>
+                    <img src={require('../images/fair_employer.png')} align='left'/>
+                  </div>
+                  <div className='col-lg-8'>
+                    <p style={{fontSize: '2em'}}>Learn more about being a fair domestic employer.</p>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-lg-4' style={{paddingTop: 40}}>
+                    <img src={require('../images/bill.png')}/>
+                  </div>
+                  <div className='col-lg-8' style={{paddingTop: 40}}>
+                    <p style={{fontSize: '2em'}}>Read the Domestic Workers Bill of Rights</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     </div>;
   }
 }
--- pages/NotFoundPage.js
+++ pages/NotFoundPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class NotFoundPage extends React.Component {
   render() {
-    return <div className='text-center'>
-      404
+    return <div className='bg-brand-black' style={{color: 'white', minHeight: 800, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-lg-6 text-right'>
+            <img src={require('../images/404.png')} align='left'/>
+          </div>
+          <div className='col-lg-6'>
+            <p style={{fontSize: '4em'}}>Oops, looks like we
+                  missed the mark.</p>
+            <Link to='home' className='btn btn-link btn-lg btn-custom' style={{marginTop: 80, marginBottom: 80}}>
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+
     </div>
   }
 }
