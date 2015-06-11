1dbbd60ba6d52eb04180207fbbd3caa22a56f597
Volkan Unsal <spocksplanet@gmail.com>
2015-06-11 19:51:19 -0400

    fix the end page

--- components/Section10.js
+++ components/Section10.js
@@ -55,39 +55,44 @@ export default class SectionPage extends React.Component {
 
     let {contract} = this.props;
 
-    let printPage = <div className='hidden-print'>
-        <div className='text-center'>
-          <h3>Congratulations, you did it! You’ve reached the end of the agreement. Be sure to review your information by using the tabs before printing it, making multiple copies and signing it!</h3>
-          <p>
+    let printPage = <div>
+        <div className='text-center hidden-print'>
+          <div className='text-center'>
+            <h3>Congratulations, you did it! You’ve reached the end of the agreement. Be sure to review your information by using the tabs before printing it, making multiple copies and signing it!</h3>
+            <p>
+              <a
+                onClick={this.printForm.bind(this)}
+                className='btn btn-lg btn-primary'
+                style={{marginTop: 20, width: 200}}>Print</a>
+            </p>
+          </div>
+
+          <div className='text-center'>
+            <p>
+              <span className='fa fa-info-circle'/> Remember: You can always edit an agreement that you’ve printed with a pen.</p>
+            <h3>
+              We need your help to pay for this tool and keep it up to date. Please make a $10.00 donation (or other amount) and/or give us your email address so we can send you the latest updates and access to new tools, like this one.
+              You can print and download your agreement once your donation clears.
+            </h3>
+            <p>
+              <a
+                onClick={this.openChargeModal.bind(this)}
+                className='btn btn-lg btn-warning'
+                style={{marginTop: 20, width: 200}}>Donate</a>
+            </p>
+          </div>
+
+          <div className='text-center'>
+            <h3>Your agreement is ready! Please remember that you can amend the agreement with a pen, on the document itself, after you’ve printed it out.</h3>
             <a
               onClick={this.printForm.bind(this)}
               className='btn btn-lg btn-primary'
               style={{marginTop: 20, width: 200}}>Print</a>
-          </p>
 
-          <p>
-            <span className='fa fa-info-circle'/> Remember: You can always edit an agreement that you’ve printed with a pen.</p>
-          <h3>
-            We need your help to pay for this tool and keep it up to date. Please make a $10.00 donation (or other amount) and/or give us your email address so we can send you the latest updates and access to new tools, like this one.
-            You can print and download your agreement once your donation clears.
-          </h3>
-          <p>
-            <a
-              onClick={this.openChargeModal.bind(this)}
-              className='btn btn-lg btn-warning'
-              style={{marginTop: 20, width: 200}}>Donate</a>
-          </p>
-        </div>
+            <h4>Thank you</h4>
+            <p>for taking the steps toward creating an agreement between you and your domestic worker!</p>
+          </div>
 
-        <div className='text-center'>
-          <h3>Your agreement is ready! Please remember that you can amend the agreement with a pen, on the document itself, after you’ve printed it out.</h3>
-          <a
-            onClick={this.printForm.bind(this)}
-            className='btn btn-lg btn-primary'
-            style={{marginTop: 20, width: 200}}>Print</a>
-
-          <h4>Thank you</h4>
-          <p>for taking the steps toward creating an agreement between you and your domestic worker!</p>
         </div>
 
       <Contract {...contract} />
