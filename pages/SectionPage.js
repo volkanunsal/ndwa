6c3c4a2d43c4e2597169ce2eca88f89317639b42
Volkan Unsal <spocksplanet@gmail.com>
2015-12-02 18:33:26 -0500

    Add Facebook code

--- pages/SectionPage.js
+++ pages/SectionPage.js
@@ -11,6 +11,15 @@ export default class SectionPage extends React.Component {
       window.onbeforeunload = function() {
         return 'For this BETA version, closing this tab means you will lose the information you typed in.';
       }
+
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '788408467948972');
+      fbq('track', "PageView");
     };
   }
 
